25e2f88ffe8295880e41c6b11cb05c46fea50af7
Chaitanya Tawade <omtawade2002@gmail.com>
2024-02-01 14:54:08 +0530

    Features Runner file

--- ParaBankTestingFramework/src/test/java/com/Runner/Runner.java
+++ ParaBankTestingFramework/src/test/java/com/Runner/Runner.java
@@ -5,6 +5,13 @@ import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+
+/*
+ * Feature Runner file , using that directly able to run whole framework.
+ * @Author chaitanya tawade (expleo pune) .
+ * @sign 01/02/2024 jdk-1.7
+ */
+
 @RunWith(Cucumber.class)
 
 public class Runner {
